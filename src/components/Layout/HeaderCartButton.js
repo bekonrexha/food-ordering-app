fbb2b906b35eda380fc819a81bb9a446c6d8ef75
bekon93 <73229765+bekon93@users.noreply.github.com>
2021-11-20 18:48:33 +0100

    add-responsiveDesign-and-checkout-page
    The responsive design for the Cart icon was added, as well as the checkout page.

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -2,17 +2,21 @@ import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 import { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context";
+import { useMediaQuery } from 'react-responsive';
 
 const HeaderCartButton = (props) => {
   const cartCtx = useContext(CartContext);
   const [bih, setBih] = useState(false);
   const { items } = cartCtx;
-
+  
   const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
-  const btnClasses = `${classes.button} ${bih ? classes.bump : ""}`;
+  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 900px)' })
+
+  const btnClasses = `${isTabletOrMobile ? classes.button__mobile : classes.button} ${bih ? classes.bump : ""}`;
+  const badgeClass = `${isTabletOrMobile ? classes.badge__mobile : classes.badge}`;
 
   useEffect(() => {
     if (items.length === 0) {
@@ -30,12 +34,12 @@ const HeaderCartButton = (props) => {
   }, [items]);
 
   return (
-    <button className={btnClasses} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick} >
       <span className={classes.icon}>
         <CartIcon />
       </span>
-      <span>Your Cart</span>
-      <span className={classes.badge}>{numberOfCartItems}</span>
+      {!isTabletOrMobile && <span>Your Cart</span>}
+      <span className={badgeClass}>{numberOfCartItems}</span>
     </button>
   );
 };
