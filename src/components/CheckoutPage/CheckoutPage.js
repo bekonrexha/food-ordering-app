3df9f94fdd9c87a67f71a1d60433e28fa7654a59
Armend <armenddika1@gmail.com>
2021-11-21 17:32:27 +0100

    changes useState instead of useRef

--- src/components/CheckoutPage/CheckoutPage.js
+++ src/components/CheckoutPage/CheckoutPage.js
@@ -7,13 +7,19 @@ const isFiveChars = (value) => value.trim().length === 5;
 
 const CheckoutPage = (props) => {
 
-const navigate = useNavigate();
+  const navigate = useNavigate();
 
   const [formInputsValidity, setFormInputsValidity] = useState({
     name: true,
     street: true,
     city: true,
     postalCode: true,
+  });
+  const [orderDetails, setOrderDetails] = useState({
+    enteredName: "",
+    enteredStreet: "",
+    enteredCity: "",
+    enteredPostalCode: "",
   });
 
   const cartCtx = useContext(CartContext);
@@ -35,23 +41,23 @@ const navigate = useNavigate();
 
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
 
-  const nameInputRef = useRef();
-  const streetInputRef = useRef();
-  const postalCodeInputRef = useRef();
-  const cityInputRef = useRef();
+  // const nameInputRef = useRef();
+  // const streetInputRef = useRef();
+  // const postalCodeInputRef = useRef();
+  // const cityInputRef = useRef();
 
   const confirmHandler = (event) => {
     event.preventDefault();
 
-    const enteredName = nameInputRef.current.value;
-    const enteredStreet = streetInputRef.current.value;
-    const enteredPostalCode = postalCodeInputRef.current.value;
-    const enteredCity = cityInputRef.current.value;
+    // const enteredName = nameInputRef.current.value;
+    // const enteredStreet = streetInputRef.current.value;
+    // const enteredPostalCode = postalCodeInputRef.current.value;
+    // const enteredCity = cityInputRef.current.value;
 
-    const enteredNameIsValid = !isEmpty(enteredName);
-    const enteredStreetIsValid = !isEmpty(enteredStreet);
-    const enteredCityIsValid = !isEmpty(enteredCity);
-    const enteredPostalCodeIsValid = isFiveChars(enteredPostalCode);
+    const enteredNameIsValid = !isEmpty(orderDetails.enteredName);
+    const enteredStreetIsValid = !isEmpty(orderDetails.enteredStreet);
+    const enteredCityIsValid = !isEmpty(orderDetails.enteredCity);
+    const enteredPostalCodeIsValid = isFiveChars(orderDetails.enteredPostalCode);
 
     setFormInputsValidity({
       name: enteredNameIsValid,
@@ -71,10 +77,10 @@ const navigate = useNavigate();
     }
 
     const userData = {
-      name: enteredName,
-      street: enteredStreet,
-      city: enteredCity,
-      postalCode: enteredPostalCode,
+      name: orderDetails.enteredName,
+      street: orderDetails.enteredStreet,
+      city: orderDetails.enteredCity,
+      postalCode: orderDetails.enteredPostalCode,
     };
 
     const submitHandler = async (userData) => {
@@ -96,16 +102,16 @@ const navigate = useNavigate();
 
   const nameControlClasses = `${classes.control} ${
     formInputsValidity.name ? "" : classes.invalid
-  }`;
+    }`;
   const streetControlClasses = `${classes.control} ${
     formInputsValidity.street ? "" : classes.invalid
-  }`;
+    }`;
   const postalCodeControlClasses = `${classes.control} ${
     formInputsValidity.postalCode ? "" : classes.invalid
-  }`;
+    }`;
   const cityControlClasses = `${classes.control} ${
     formInputsValidity.city ? "" : classes.invalid
-  }`;
+    }`;
 
   return (
     <div className={classes.mainpage}>
@@ -117,24 +123,40 @@ const navigate = useNavigate();
         <form className={classes.form} onSubmit={confirmHandler}>
           <div className={nameControlClasses}>
             <label htmlFor="name">Your Name</label>
-            <input type="text" id="name" ref={nameInputRef} />
+            <input type="text" id="name"
+              // ref={nameInputRef}
+              value={orderDetails.enteredName}
+              onChange={(event) => setOrderDetails({ ...orderDetails, enteredName: event.target.value })}
+            />
             {!formInputsValidity.name && <p>Please enter a valid name!</p>}
           </div>
           <div className={streetControlClasses}>
             <label htmlFor="street">Street</label>
-            <input type="text" id="street" ref={streetInputRef} />
+            <input type="text" id="street"
+              // ref={streetInputRef} 
+              value={orderDetails.enteredStreet}
+              onChange={(event) => setOrderDetails({ ...orderDetails, enteredStreet: event.target.value })}
+            />
             {!formInputsValidity.street && <p>Please enter a valid street!</p>}
           </div>
           <div className={postalCodeControlClasses}>
             <label htmlFor="postal">Postal Code</label>
-            <input type="text" id="postal" ref={postalCodeInputRef} />
+            <input type="text" id="postal"
+              // ref={postalCodeInputRef}
+              value={orderDetails.enteredPostalCode}
+              onChange={(event) => setOrderDetails({ ...orderDetails, enteredPostalCode: event.target.value })}
+            />
             {!formInputsValidity.postalCode && (
               <p>Please enter a valid postal code (5 characters long)!</p>
             )}
           </div>
           <div className={cityControlClasses}>
             <label htmlFor="city">City</label>
-            <input type="text" id="city" ref={cityInputRef} />
+            <input type="text" id="city"
+              // ref={cityInputRef} 
+              value={orderDetails.enteredCity}
+              onChange={(event) => setOrderDetails({ ...orderDetails, enteredCity: event.target.value })}
+            />
             {!formInputsValidity.city && <p>Please enter a valid city!</p>}
           </div>
           <div className={classes.actions}>
